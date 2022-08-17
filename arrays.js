6016f81bf7be77498db992da9efc2de1e4177137
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-17 17:26:22 +0530

    js array methods

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var myArray = ['h', 'e', 'l', 'l', 'o'];
+
+// first element
+console.log(myArray[0]);  // "h"
+
+// second element
+console.log(myArray[1]); // "e"
+
+
+
+var dailyActivities = ['eat', 'sleep'];
+
+// add an element at the end
+dailyActivities.push('exercise');
+
+console.log(dailyActivities); //  ['eat', 'sleep', 'exercise']
+
+
+
+var dailyActivities = ['eat', 'sleep'];
+
+//add an element at the start
+dailyActivities.unshift('work');                                   //unshift()method adds an element 
+
+console.log(dailyActivities); // ['work', 'eat', 'sleep']
+
+
+
+
+var dailyActivities = [ 'eat', 'sleep'];
+
+// this will add the new element 'exercise' at the 3 index
+dailyActivities[3] = 'exercise';
+
+console.log(dailyActivities); // ["eat", "sleep", undefined, "exercise"]
+
+
+
+var dailyActivities = ['work', 'eat', 'sleep', 'exercise'];
+
+// remove the last element
+dailyActivities.pop();
+console.log(dailyActivities); // ['work', 'eat', 'sleep']
+
+// remove the last element from ['work', 'eat', 'sleep']
+const removedElement = dailyActivities.pop();
+
+//get removed element
+console.log(removedElement); // 'sleep'
+console.log(dailyActivities);  // ['work', 'eat']
